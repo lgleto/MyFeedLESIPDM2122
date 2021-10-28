0b75ba54f96410a1e7ae0823061781de15ab2a2c
Lourenço Gomes <leto.lg@gmail.com>
2021-10-28 07:57:37 +0100

    - Intents - Menus

--- app/src/main/java/ipca/example/myfeed/ArticleDetailActivity.kt
+++ app/src/main/java/ipca/example/myfeed/ArticleDetailActivity.kt
@@ -1,23 +1,28 @@
 package ipca.example.myfeed
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.widget.Toast
 import org.json.JSONObject
 
 class ArticleDetailActivity : AppCompatActivity() {
 
     private lateinit var webView : WebView
+    private lateinit var article : Article
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
 
         val jsonString =  intent.extras?.getString("article")
-        val article = Article.fromJson(JSONObject(jsonString))
+        article = Article.fromJson(JSONObject(jsonString))
 
         title = article.title
 
@@ -30,5 +35,28 @@ class ArticleDetailActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.web_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        return when (item.itemId){
+            R.id.action_share -> {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_SUBJECT, article.title)
+                intent.putExtra(Intent.EXTRA_TEXT, article.url)
+                startActivity((Intent.createChooser(intent, "My Feed")))
+                true
+            }
+            R.id.action_open_browser -> {
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(article.url)
+                startActivity(i)
+                true
+            }
+            else ->  super.onOptionsItemSelected(item)
+        }
+
+
     }
 }
